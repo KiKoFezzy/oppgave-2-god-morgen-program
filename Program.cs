8da99411ac00502e4152c6384cf42f1375c3c22b
Fezzy <victor1.26pg@gmail.com>
2026-05-08 09:03:01 +0200

    god morgen basert på tiden i virkeligheten ferdig2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        int hour = DateTime.Now.Hour; // Get the current hour (0-23)
+
+        if (hour < 12)
+        {
+            Console.WriteLine("God morgen!");
+        }
+        else if (hour < 17)
+        {
+            Console.WriteLine("God dag!");
+        }
+        else
+        {
+            Console.WriteLine("God kveld!");
+        }
+    }
+}
