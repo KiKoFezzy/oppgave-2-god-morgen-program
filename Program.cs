5e6c8357d61df87f59454abfc4bc1c6ee49ddf9f
Fezzy <victor1.26pg@gmail.com>
2026-05-08 09:09:11 +0200

    lagt til forklaring på norsk

--- Program.cs
+++ Program.cs
@@ -2,19 +2,19 @@
 {
     static void Main()
     {
-        int hour = DateTime.Now.Hour; // Get the current hour (0-23)
+        int hour = DateTime.Now.Hour; //hent den virkelige tiden (0-23)
 
         if (hour < 12)
         {
-            Console.WriteLine("God morgen!");
+            Console.WriteLine("God morgen!"); //om klokka er under 12, print ut "god morgen" i terminalen
         }
         else if (hour < 17)
         {
-            Console.WriteLine("God dag!");
+            Console.WriteLine("God dag!"); //om klokka er mellom 12 og 17, print ut "god dag" i terminalen
         }
         else
         {
-            Console.WriteLine("God kveld!");
+            Console.WriteLine("God kveld!"); //om klokka er etter 17, print ut "god kveld" i terminalen
         }
     }
 }
